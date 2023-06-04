375c2f8c490608962fd22a4c9656425899d47208
NOKKY726 <nkfrom.github@gmail.com>
2023-06-05 03:51:18 +0900

    1 min -> 10 s

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from fractions import Fraction
 from pathlib import Path
 
 from PIL import Image, ImageDraw, ImageFont, ImageGrab
@@ -21,23 +22,27 @@ class App(tk.Tk):
         self.canvas.pack()
 
         Path("Screenshot").mkdir(exist_ok=True)
-        for i, image in enumerate([Image.new("RGBA", (384, 216), "black") for i in range(10)]):
-            ImageDraw.Draw(image).text((10, 0), str(i), "white", ImageFont.truetype("arial.ttf", 72))
+        for i, image in enumerate([Image.new("RGBA", (384, 216), "black") for _ in range(60)]):
+            ImageDraw.Draw(image).text((4, 0), str(Fraction(i, 6)), "white", ImageFont.truetype("arial.ttf", 36))
             image.save(f"Screenshot/{i}.png")
-        ImageGrab.grab().resize((384, 216)).save("Screenshot/10.png")
+        ImageGrab.grab().resize((384, 216)).save("Screenshot/60.png")
 
         self.display_image = tk.PhotoImage(file="Screenshot/0.png")
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.display_image)
 
-        self.after(60000, self.update_canvas)
+        self.after(10000, self.update_canvas)
 
     def update_canvas(self):
         Path("Screenshot/0.png").unlink()
-        for i in range(10):
+        for i in range(60):
             Path(f"Screenshot/{i + 1}.png").rename(f"Screenshot/{i}.png")
-        ImageGrab.grab().resize((384, 216)).save("Screenshot/10.png")
+        ImageGrab.grab().resize((384, 216)).save("Screenshot/60.png")
 
         self.display_image = tk.PhotoImage(file="Screenshot/0.png")
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.display_image)
 
-        self.after(60000, self.update_canvas)
+        self.after(10000, self.update_canvas)
+
+
+if __name__ == "__main__":
+    main()
