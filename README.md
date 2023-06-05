dbca16814db3d0450b0259da2becd053ce777a45
NOKKY726 <nkfrom.github@gmail.com>
2023-06-05 23:26:45 +0900

    added readme and license

---
+++ README.md
@@ -0,0 +1,12 @@
+# screenshot-before-10-min
+
+## Inspired
+https://twitter.com/blu3mo/status/1664743883928944642
+
+## Usage
+```
+python -m venv .venv
+.venv/Scripts/activate
+pip install -r requirements.txt
+python app.py
+```
--- app.py
+++ app.py
@@ -21,24 +21,24 @@ class App(tk.Tk):
         self.canvas = tk.Canvas(self, width=384, height=216)
         self.canvas.pack()
 
-        Path("Screenshot").mkdir(exist_ok=True)
+        Path("Screenshots").mkdir(exist_ok=True)
         for i, image in enumerate([Image.new("RGBA", (384, 216), "black") for _ in range(60)]):
             ImageDraw.Draw(image).text((4, 0), str(Fraction(i, 6)), "white", ImageFont.truetype("arial.ttf", 36))
-            image.save(f"Screenshot/{i}.png")
-        ImageGrab.grab().resize((384, 216)).save("Screenshot/60.png")
+            image.save(f"Screenshots/{i}.png")
+        ImageGrab.grab().resize((384, 216)).save("Screenshots/60.png")
 
-        self.display_image = tk.PhotoImage(file="Screenshot/0.png")
+        self.display_image = tk.PhotoImage(file="Screenshots/0.png")
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.display_image)
 
         self.after(10000, self.update_canvas)
 
     def update_canvas(self):
-        Path("Screenshot/0.png").unlink()
+        Path("Screenshots/0.png").unlink()
         for i in range(60):
-            Path(f"Screenshot/{i + 1}.png").rename(f"Screenshot/{i}.png")
-        ImageGrab.grab().resize((384, 216)).save("Screenshot/60.png")
+            Path(f"Screenshots/{i + 1}.png").rename(f"Screenshots/{i}.png")
+        ImageGrab.grab().resize((384, 216)).save("Screenshots/60.png")
 
-        self.display_image = tk.PhotoImage(file="Screenshot/0.png")
+        self.display_image = tk.PhotoImage(file="Screenshots/0.png")
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.display_image)
 
         self.after(10000, self.update_canvas)
