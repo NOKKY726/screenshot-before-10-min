28dfa426b457b83472b715419509c47afe72464e
NOKKY726 <nkfrom.github@gmail.com>
2023-06-09 22:37:56 +0900

    manage images by list

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import tkinter as tk
-from fractions import Fraction
-from pathlib import Path
 
-from PIL import Image, ImageDraw, ImageFont, ImageGrab
+from PIL import Image, ImageDraw, ImageFont, ImageGrab, ImageTk
 
 
 def main():
@@ -21,27 +19,29 @@ class App(tk.Tk):
         self.canvas = tk.Canvas(self, width=384, height=216)
         self.canvas.pack()
 
-        Path("Screenshots").mkdir(exist_ok=True)
-        for i, image in enumerate([Image.new("RGBA", (384, 216), "black") for _ in range(60)]):
-            ImageDraw.Draw(image).text((10, 0), f"{i // 6} + {Fraction(i % 6, 6)}", "white", ImageFont.truetype("arial.ttf", 36))
-            image.save(f"Screenshots/{i}.png")
-        ImageGrab.grab().resize((384, 216)).save("Screenshots/60.png")
+        self.screenshots = [Image.new("RGBA", (384, 216), "black") for _ in range(601)]
+        for i, image in enumerate(self.screenshots):
+            ImageDraw.Draw(image).text(
+                (10, 0), f"{i // 60} min {i % 60} s",
+                "white", ImageFont.truetype("arial.ttf", 36),
+            )
+            self.screenshots[i] = image
+        self.screenshots[600] = ImageGrab.grab().resize((384, 216))
 
-        self.display_image = tk.PhotoImage(file="Screenshots/0.png")
+        self.display_image = ImageTk.PhotoImage(self.screenshots[0])
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.display_image)
 
-        self.after(10000, self.update_canvas)
+        self.after(1000, self.update_canvas)
 
     def update_canvas(self):
-        Path("Screenshots/0.png").unlink()
-        for i in range(60):
-            Path(f"Screenshots/{i + 1}.png").rename(f"Screenshots/{i}.png")
-        ImageGrab.grab().resize((384, 216)).save("Screenshots/60.png")
+        for i in range(600):
+            self.screenshots[i] = self.screenshots[i + 1]
+        self.screenshots[600] = ImageGrab.grab().resize((384, 216))
 
-        self.display_image = tk.PhotoImage(file="Screenshots/0.png")
+        self.display_image = ImageTk.PhotoImage(self.screenshots[0])
         self.canvas.create_image(0, 0, anchor=tk.NW, image=self.display_image)
 
-        self.after(10000, self.update_canvas)
+        self.after(1000, self.update_canvas)
 
 
 if __name__ == "__main__":
